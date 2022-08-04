4aa71a3fc3597f1ab8b983adb11258a43886870f
neidenfree <avstrelnikov95@gmail.com>
2022-08-04 16:50:15 +0700

    Added getitem for deque and some tests

--- stack.py
+++ stack.py
@@ -17,8 +17,33 @@ class StackElement:
             raise TypeError("Ебучий дескриптор нихуя не справился, пришлось городить такую вот хуету")
 
 
+class DequeElement:
+    link_prev = GeneralDescriptor()
+    link_next = GeneralDescriptor()
+    data = GeneralDescriptor()
+
+    def __init__(self, data, link_prev: DequeElement = None, link_next: DequeElement = None):
+        self.data = data
+        if link_next is None or type(link_next) is DequeElement:
+            self.link_next = link_next
+        else:
+            raise TypeError("Next link must be of class DequeElement!")
+
+        if link_prev is None or type(link_prev) is DequeElement:
+            self.link_prev = link_prev
+        else:
+            raise TypeError("Prev link must be of class DequeElement!")
+
+    def __str__(self):
+        return str(self.data)
+
+
 class StackElementDescriptor(GeneralDescriptor):
     allowed_type = StackElement
+
+
+class DequeElementDescriptor(GeneralDescriptor):
+    allowed_type = DequeElement
 
 
 class Stack:
@@ -26,11 +51,11 @@ class Stack:
     head = StackElementDescriptor()
 
     def __init__(self, head: StackElement = None):
-        self.__count = 0
+        self._count = 0
         self.head = head
 
     def __len__(self):
-        return self.__count
+        return self._count
 
     def __iter__(self):
         new_s = Stack(self.head)
@@ -71,7 +96,7 @@ class Stack:
 
     def __delitem__(self, key):
         integer_type(key)
-        if key >= self.__count:
+        if key >= self._count:
             raise IndexError
         temp = self.head
         ind = 0
@@ -90,7 +115,7 @@ class Stack:
 
     def __setitem__(self, key, value):
         integer_type(key)
-        if key >= self.__count:
+        if key >= self._count:
             raise IndexError
         if key == 0:
             self.head.data = value
@@ -120,7 +145,7 @@ class Stack:
 
     def clear(self):
         self.head = None
-        self.__count = 0
+        self._count = 0
 
     def push(self, elem):
         if type(elem) == StackElement:
@@ -130,7 +155,7 @@ class Stack:
             se = StackElement(elem)
         se.link = self.head
         self.head = se
-        self.__count += 1
+        self._count += 1
 
     def push_range(self, elements: Iterable):
         for elem in elements:
@@ -141,5 +166,145 @@ class Stack:
             raise IndexError("The stack is empty")
         res = self.head.data
         self.head = self.head.link
-        self.__count -= 1
+        self._count -= 1
         return res
+
+
+class Queue(Stack):
+    """Очередь, быстрая на доступ к элементам. Добавление элементов в очередь реализовано неоптимально."""
+
+    def push(self, elem):
+        if type(elem) == StackElement:
+            sm = StackElement(elem.value)
+        else:
+            sm = StackElement(elem)
+
+        if self.head is None:
+            self.head = sm
+            return
+
+        temp = self.head
+        while temp.link is not None:
+            temp = temp.link
+        temp.link = sm
+        self._count += 1
+
+
+class Deque:
+    head = DequeElementDescriptor()
+    tail = DequeElementDescriptor()
+
+    def __init__(self, head: DequeElement = None, tail: DequeElement = None):
+        self.head = head
+        self.tail = tail
+        self._count = 0
+
+    def __len__(self):
+        return self._count
+
+    def __iter__(self):
+        dq = Deque(self.head, self.tail)
+        setattr(dq, 'stop', False)
+        return dq
+
+    def __next__(self):
+        if not hasattr(self, 'stop'):
+            raise AttributeError("Iterator didn't set attribute stop to this object!")
+        if self.stop:
+            raise StopIteration
+        res = self.tail
+        self.tail = self.tail.link_next
+        if self.tail is None:
+            self.stop = True
+
+        return res.data
+
+    def __str__(self):
+        if self.head is None:
+            return ""
+        return " <=> ".join([str(x) for x in self])
+
+    def __reversed__(self):
+        if self.tail is None or self._count == 1:
+            return self
+        head = self.tail
+        while head is not None:
+            temp = head.link_prev
+            head.link_prev = head.link_next
+            head.link_next = temp
+            head = head.link_prev
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+        # self.head, self.tail = self.tail, self.head
+
+    def __getitem__(self, item):
+        integer_type(item)
+        if item >= len(self) or item < 0:
+            raise IndexError
+        # Если ключ
+        if item < len(self) // 2:
+            tail = self.tail
+            ind = 0
+            while tail is not None:
+                if ind == item:
+                    return tail.data
+                else:
+                    tail = tail.link_next
+                    ind += 1
+        else:
+            head = self.head
+            ind = len(self) - 1
+            while head is not None:
+                if ind == item:
+                    return head.data
+                else:
+                    head = head.link_prev
+                    ind -= 1
+
+    def create_first(self, value):
+        if self.head is None:
+            dem = DequeElement(value)
+            self.head = dem
+            self.tail = dem
+            self._count += 1
+            return True
+        return False
+
+    def clear(self):
+        self.head = None
+        self.tail = None
+        self._count = 0
+
+    def push_front(self, value) -> None:
+        if self.create_first(value):
+            return
+
+        dem = DequeElement(value)
+        dem.link_prev = self.head
+        self.head.link_next = dem
+        self.head = dem
+        self._count += 1
+
+    def push_back(self, value):
+        if self.create_first(value):
+            return
+
+        dem = DequeElement(value)
+        dem.link_next = self.tail
+        self.tail.link_prev = dem
+        self.tail = dem
+        self._count += 1
+
+    def push_front_range(self, value: Iterable):
+        if not isinstance(value, Iterable):
+            raise TypeError(f"{value} is not Iterable!")
+        for el in value:
+            self.push_front(el)
+
+    def push_back_range(self, value: Iterable):
+        if not isinstance(value, Iterable):
+            raise TypeError(f"{value} is not Iterable!")
+        for el in value:
+            self.push_back(el)
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from stack import Stack
+from stack import Stack, Queue, Deque
 
 
 class TestStack(unittest.TestCase):
@@ -31,7 +31,7 @@ class TestStack(unittest.TestCase):
         # Return TypeError for not integer index
         self.assertRaises(TypeError, fuck('asdasd'))
         self.assertRaises(TypeError, fuck(12.4))
-        self.assertRaises(TypeError, fuck((2, )))
+        self.assertRaises(TypeError, fuck((2,)))
 
         # Стандартные процедуры удаления
         self.s.push_range(range(0, 10))
@@ -64,8 +64,116 @@ class TestStack(unittest.TestCase):
         lst[-2] = 100
         self.assertListEqual(list(reversed(self.s)), lst)
 
+    def test_clear(self):
+        sz = self.s.__sizeof__()
+        self.s.push_range(range(20))
+        self.s.clear()
+        self.assertEqual(sz, self.s.__sizeof__())
+
     def tearDown(self) -> None:
         del self.s
+
+
+class TestQueue(unittest.TestCase):
+    def setUp(self) -> None:
+        self.q = Queue()
+
+    def test_push(self) -> None:
+        self.assertListEqual(list(self.q), [])
+        self.q.push(1)
+        self.assertListEqual(list(self.q), [1])
+        self.q.push(2)
+        self.assertListEqual(list(self.q), [1, 2])
+        self.q.push(3)
+        self.assertListEqual(list(self.q), [1, 2, 3])
+
+    def test_push_range(self) -> None:
+        self.assertEqual(list(self.q), [])
+        self.q.push_range(range(1, 10))
+        self.assertEqual([*range(1, 10)], list(self.q))
+
+    def test_set_item(self):
+        self.q.push_range(range(0, 10))
+        self.q[0] = 100
+        lst = list(range(0, 10))
+        lst[0] = 100
+        self.assertListEqual(lst, list(self.q))
+
+    def tearDown(self) -> None:
+        del self.q
+
+
+class TestDeque(unittest.TestCase):
+    def setUp(self) -> None:
+        self.d = Deque()
+
+    def test_push_front(self):
+        self.d.push_front(2)
+        self.assertEqual(2, self.d.tail.data)
+        self.assertEqual(2, self.d.head.data)
+        self.d.push_front(1)
+        self.assertEqual(2, self.d.tail.data)
+        self.assertEqual(1, self.d.head.data)
+
+    def test_push_back(self):
+        self.d.push_back(2)
+        self.assertEqual(2, self.d.tail.data)
+        self.assertEqual(2, self.d.head.data)
+        self.d.push_back(1)
+        self.assertEqual(1, self.d.tail.data)
+        self.assertEqual(2, self.d.head.data)
+
+    def test_iter(self):
+        self.d.push_front(1)
+        self.d.push_front(2)
+        self.d.push_front(3)
+        self.d.push_front(4)
+        it = iter(self.d)
+        z = next(it)
+        self.assertEqual(z, 1)
+        z = next(it)
+        self.assertEqual(z, 2)
+        z = next(it)
+        self.assertEqual(z, 3)
+
+    def test_push_front_back_range(self):
+        self.d.push_front_range(range(5))
+        self.assertEqual(list(self.d), [*range(5)])
+        lst = list(reversed(range(10, 15)))
+        lst.extend(list(range(5)))
+        self.d.push_back_range(range(10, 15))
+        self.assertListEqual(list(self.d), lst)
+
+    def test_reversed(self):
+        self.d.push_front_range(range(5))
+        reversed(self.d)
+        d_lst = list(self.d)
+        lst = list(reversed(range(5)))
+        self.assertEqual(d_lst, lst)
+
+        self.d.clear()
+        # self.d.push_front(5)
+        reversed(self.d)
+        self.assertEqual(self.d.tail, None)
+
+    def test_getitem(self):
+        def func(index):
+            def wrapper(*args, **kwargs):
+                return self.d[index]
+
+            return wrapper
+
+        self.d.push_front_range(range(10))
+        lst = list(range(10))
+        self.assertEqual(self.d[9], lst[9])
+        self.assertRaises(TypeError, func(2.4))
+        self.assertRaises(IndexError, func(10))
+        self.assertRaises(IndexError, func(-1))
+        for i in range(10):
+            self.assertEqual(self.d[i], lst[i])
+        
+    def tearDown(self) -> None:
+        del self.d
 
 
 if __name__ == "__main__":
